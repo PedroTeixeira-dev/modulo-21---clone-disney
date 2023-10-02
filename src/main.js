b1d10a0cbf1873d2f03ab0b695a7ead7d6eca38c
Pedro Teixeira <pedro15at@gmail.com>
2023-10-02 11:21:09 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
     const questions = document.querySelectorAll('[data-faq-question]')
+
+    const heroSection = document.querySelector('.hero')
+    const alturaHero = heroSection.clientHeight
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = this.window.scrollY
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementos()
+        }
+        else {
+            exibeElementos()
+        }
+    })
     
+
+    // para decao de atracoes, programacao das abas
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton
@@ -14,10 +30,22 @@ document.addEventListener('DOMContentLoaded', function(){
         })
     }
 
+
+    //secao faq accordion
     for(let i =0; i < questions.length; i++){
         questions[i].addEventListener('click',abreOuFechaResposta)
     }
 })
+
+function ocultaElementos() {
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementos () {
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open'
